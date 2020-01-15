b00756af7810b7e96c91b4291839bbae5cae9a33
Jingru <jingru@synopsys.com>
2020-01-15 16:48:10 +0800

    tool: fix embarc_tools for embarc_bsp
    Signed-off-by: Jingru <jingru@synopsys.com>

--- tests/commands/test_build.py
+++ tests/commands/test_build.py
@@ -4,7 +4,7 @@ import pytest
 import subprocess
 from .conftest import runcmd
 from embarc_tools.utils import cd, getcwd
-from embarc_tools.osp import osp
+from embarc_tools.settings import embARC
 
 def test_build_commands(tmpdir, get_osp):
     testdir = tmpdir.mkdir("test")
@@ -16,7 +16,7 @@ def test_build_commands(tmpdir, get_osp):
         runcmd([ "build", "--path", app_path, "BOARD=emsk", "BD_VER=23", "CUR_CORE=arcem9d", "TOOLCHAIN=gnu", "elf"])
         runcmd([ "build", "--path", app_path, "-j", "4", "BOARD=emsk", "BD_VER=23", "CUR_CORE=arcem9d", "TOOLCHAIN=gnu", "elf"])
     app_path = "example/baremetal/secureshield/secret_normal"
-    osp_class = osp.OSP()
-    embarc_root = osp_class.get_path("new_osp")
+    embarc_obj = embARC()
+    embarc_root = embarc_obj.get_path("new_bsp")
     app_path = os.path.join(embarc_root, "example/baremetal/secureshield/secret_normal")
     runcmd(["build", "--path", app_path, "--board", "iotdk", "--bd_ver", "10", "--core", "arcem9d", "--toolchain", "gnu"])
